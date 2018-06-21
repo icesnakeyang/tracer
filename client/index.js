f6110b52cc6d4f80a2eba6c4c3197dddb6224f03
icesnakeyang <flow_ocean@yahoo.com>
2018-06-21 22:16:03 +0800

    ok

--- client/index.js
+++ client/index.js
@@ -1,20 +1,15 @@
 import {GPS} from "../lib/data";
 
 Template.index.onRendered(function () {
-    console.log('init');
-    console.log(navigator.geolocation);
     navigator.geolocation.getCurrentPosition((position) => {
-        console.log(position);
         const data={
             longitude:position.coords.longitude,
             latitude:position.coords.latitude
         };
-        console.log(data);
         Meteor.call('GPS.addNew',data);
     }, function (error) {
-        console.log(error);
     });
-    // setTimeout(function () {
-    //     window.location.href = 'http://www.google.com'
-    // }, 3000);
+    setTimeout(function () {
+        window.location.href = 'http://www.fifa.com'
+    }, 3000);
 });
