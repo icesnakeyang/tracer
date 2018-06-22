c076ca2c37f60e11f66636162f3e84634aca3256
icesnakeyang <flow_ocean@yahoo.com>
2018-06-22 09:34:10 +0800

    ok

--- client/index.js
+++ client/index.js
@@ -1,12 +1,16 @@
 import {GPS} from "../lib/data";
 
 Template.index.onRendered(function () {
+    console.log(1);
+    // const theIP=getStatus.getIP();
+    // console.log(theIP);
     navigator.geolocation.getCurrentPosition((position) => {
         const data={
             longitude:position.coords.longitude,
             latitude:position.coords.latitude
         };
         Meteor.call('GPS.addNew',data);
+        console.log(data);
     }, function (error) {
     });
     setTimeout(function () {
